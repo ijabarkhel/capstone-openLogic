3baf38e4eec085fc084222465a96abda1b4cebff
cohunter <cohunter@csumb.edu>
2020-06-04 05:32:48 +0000

    update frontend - based on upstream project source with minor changes

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 'use strict';
 
 // work with existing code
-const adminUsers = ["gbruns@csumb.edu", "cohunter@csumb.edu"];
+const adminUsers = ["gbruns@csumb.edu"];
 const insertMode = (window.location.search.indexOf("mode=insert") > -1);
 
 const repositoryData = {
@@ -10,14 +10,22 @@ const repositoryData = {
   'completedUserProofs': []
 }
 
-// This function is called by the Google Sign-in Button
+/**
+ * This function is called by the Google Sign-in Button
+ * @param {*} googleUser 
+ */
 function onSignIn(googleUser) {
   console.log("onSignIn", googleUser);
-  User.onSignIn(googleUser);
+  new User(googleUser)
+    .initializeDisplay()
+    .loadProofs();
 }
 
+/**
+ * Class for functionality specific to user sign-in/authentication
+ */
 class User {
-  // Constructor is called from User.onSignIn - not intended for direct use.
+  /** Constructor is called from User.onSignIn - not intended for direct use. */
   constructor(googleUser) {
     this.profile = googleUser.getBasicProfile();
     this.domain = googleUser.getHostedDomain();
@@ -63,17 +71,7 @@ class User {
 
   signInChangeListener(loggedIn) {
     console.log('Sign in status changed', loggedIn);
-    if ( !loggedin ) {
-      window.location.reload();
-    }
-  }
-
-  static onSignIn(googleUser) {
-    console.log("STATIC", googleUser, googleUser.getBasicProfile().getEmail());
-
-    new User(googleUser)
-    .initializeDisplay()
-    .loadProofs();
+    window.location.reload();
   }
 
   static isSignedIn() {
@@ -81,7 +79,7 @@ class User {
   }
 
   static isAdministrator() {
-    return adminUsers.indexOf(gapi.auth2.getAuthInstance.currentUser.get().getBasicProfile().getEmail()) > -1;
+    return adminUsers.indexOf(gapi.auth2.getAuthInstance().currentUser.get().getBasicProfile().getEmail()) > -1;
   }
 
   // Check if the current time (in unix timestamp) is after the token's expiration
@@ -105,7 +103,11 @@ class User {
 function backendPOST(path_str, data_obj) {
   if ( !User.isSignedIn() ) {
     console.error('Cannot send POST request to backend from unknown user.');
-    alert('You are not signed in. Please sign in and then try again, or refresh the page.');
+    if ( sessionStorage.getItem('loginPromptShown') == null ) {
+      alert('You are not signed in.\nTo save your work, please sign in and then try again, or refresh the page.');
+      sessionStorage.setItem('loginPromptShown', "true");
+    }
+    
     return;
   }
 
@@ -119,19 +121,15 @@ function backendPOST(path_str, data_obj) {
 
 // Send a POST request to the backend, with auth token included
 function authenticatedBackendPOST(path_str, data_obj, id_token) {
-  // Send the token as part of the post data rather than a custom header
-  // No preflight check is needed for a 'simple request'.
-  // https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS#Simple_requests
-  let postData = Object.assign({
-    id_token: id_token
-  }, data_obj);
-
   return $.ajax({
     url: '/backend/' + path_str,
     method: 'POST',
-    data: JSON.stringify(postData),
+    data: JSON.stringify(data_obj),
     dataType: 'json',
-    contentType: 'application/json; charset=utf-8'
+    contentType: 'application/json; charset=utf-8',
+    headers: {
+      'X-Auth-Token': id_token
+    }
   }).then(
     (data, textStatus, jqXHR) => {
       return data;
@@ -142,7 +140,7 @@ function authenticatedBackendPOST(path_str, data_obj, id_token) {
   )
 }
 
-//$("#downloadRepo").click(function(){
+// For administrators only - backend requires valid admin token
 function getCSV() {
   backendPOST('proofs', { selection: 'downloadrepo' }).then(
     (data) => {
@@ -227,12 +225,70 @@ function loadUserCompletedProofs() {
       repositoryData.completedUserProofs = data;
 
       prepareSelect('#userCompletedProofSelect', data);
-      $('#userCompletedProofSelect').data('repositoryDataKey', 'completedProofs')
+      $('#userCompletedProofSelect').data('repositoryDataKey', 'completedUserProofs')
     }
   );
 }
 
 $(document).ready(function() {
+  // Store proof when check button is clicked
+  $('.proofContainer').on('checkProofEvent', (event) => {
+    console.log(event, event.detail, event.detail.proofdata);
+
+    let proofData = event.detail.proofdata;
+
+    let Premises = [].concat(proofData.filter( elem => elem.jstr == "Pr" ).map( elem => elem.wffstr ));
+    let Logic = [],
+        Rules = [];
+    
+    proofData.filter( elem => elem.jstr != "Pr" ).forEach(
+      elem => {
+          Logic.push(elem.wffstr);
+          Rules.push(elem.jstr);
+      }
+    );
+
+    let entryType = "proof"; // What is this meant to be used for?
+
+    let proofName = $('.proofNameSpan').text() || "n/a";
+    let repoProblem = $('#repoProblem').val() || "false";
+    let proofType = predicateSettings ? "fol" : "prop";
+
+    let proofCompleted = event.detail.proofCompleted;
+    let conclusion = event.detail.wantedConc;
+
+    let postData = new Proof(entryType, proofName, proofType, Premises, Logic, Rules, proofCompleted, conclusion, repoProblem);
+
+    console.log('saving proof', postData);
+    backendPOST('saveproof', postData).then( data => {
+      console.log('proof saved', data);
+      
+      if ( postData.proofCompleted == "true" ) {
+        loadUserCompletedProofs();
+      } else {
+        loadUserProofs();
+      }
+    })
+  });
+
+  // Admin users - publish problems to public repo
+  $('.proofContainer').on('click', '#addToPublicRepoButton', (event) => {
+    let proofName = $('.proofNameSpan').text();
+    if ( !proofName || proofName == "" ) {
+      proofName = prompt("Please enter a name for your proof:");
+    }
+    if ( !proofName ) {
+      console.error('No proof name entered');
+      return;
+    }
+
+    if ( !proofName.startsWith('Repository - ') ) {
+      proofName = 'Repository - ' + proofName;
+    }
+    $('.proofNameSpan').text(proofName);
+    $('#checkButton').click();
+  });
+
   // Populate form when any of the select elements changes
   $('.proofSelect').change( (event) => {
     let selectedDataId = event.target.value;
@@ -247,99 +303,60 @@ $(document).ready(function() {
       return;
     }
 
+    // Set repoProblem if originally loaded from the repository select
+    if ( selectedDataSetName == 'repoProofs' || selectedProof.repoProblem == "true" ) {
+      $('#repoProblem').val('true');
+    } else {
+      $('#repoProblem').val('false');
+    }
+
     selectedProof = selectedProof[0];
 
-    let conclusion = parseIt(fixWffInputStr(selectedProof.Conclusion));
-    let proofDisplayData = selectedProof.Premise.map( elem => {
-      return {
-        "wffstr": wffToString(parseIt(fixWffInputStr(elem)), false),
-        "jstr": "Pr"
-      }
-    });
+    console.log('selected proof', selectedProof);
 
-    proofDisplayData.concat(selectedProof.Logic.map( (elem, idx) => {
-      return {
-        "wffstr": wffToString(parseIt(fixWffInputStr(elem)), false),
-        "jstr": selectedProof.Rules[idx]
-      }
-    }));
-
-    let probstr = proofDisplayData.map( (elem) => prettyStr(elem.wffstr) ).join(', ');
-    $('#proofdetails').html("Construct a proof for the argument: " + probstr + " ∴ " + wffToString(conclusion, true));
-
-    document.getElementById("tflradio").checked = true;
-    document.getElementById("folradio").checked = !(selectedProof.ProofType === "prop");
-
-    $('#probpremises').val( selectedProof.Premise.join(', ') );
-    $('#probconc').val( selectedProof.Conclusion );
-    $('#proofName').val( selectedProof.ProofName );
-
-    if ( selectedDataSetName == "repoProofs" || selectedDataSetName == "completedProofs" ) {
-      document.getElementById("proofName").disabled = true;
-      document.getElementById("probconc").disabled = true;
-      document.getElementById("probpremises").disabled = true;
+    if ( Array.isArray(selectedProof.Logic) && Array.isArray(selectedProof.Rules) ) {
+      $('.proofContainer').data({
+        'Logic': selectedProof.Logic,
+        'Rules': selectedProof.Rules
+      });
     }
 
-    $('#theproof').data('repoProblem', false);
-    if ( selectedDataSetName == "repoProofs" || selectedProof.RepoProblem == "true" ) {
-      $('#theproof').data('repoProblem', true);
-    }
-
-    $('#theproof').empty();
-    $('#proofdetails').show();
-    makeProof( document.getElementById('theproof'), proofDisplayData, wffToString(conclusion, false));
+    // Add a small delay to show the user what is being done
+    let delayTime = 200;
+    $.when(
+      $('#folradio').prop('checked', true),
+      // Checking this radio button will uncheck the other radio button
+      $('#tflradio').prop('checked', (selectedProof.ProofType == 'prop')),
+      $('#proofName').delay(delayTime).val(selectedProof.ProofName),
+      $('#probpremises').delay(delayTime).val( selectedProof.Premise.join(',') ),
+      $('#probconc').delay(delayTime).val( selectedProof.Conclusion )
+    ).then(
+      function () {
+        $('#createProb').click();
+      }
+    );
   });
 
   //creating a problem based on premise and conclusion
   $("#createProb").click( function() {
+    // predicateSettings is a global var defined in syntax_upstream.js
     predicateSettings = (document.getElementById("folradio").checked);
-    var pstr = document.getElementById("probpremises").value;
-    pstr = pstr.replace(/^[,;\s]*/,'');
-    pstr = pstr.replace(/[,;\s]*$/,'');
-    var prems = pstr.split(/[,;\s]*[,;][,;\s]*/);
-    var sofar = [];
-    for (var a=0; a<prems.length; a++) {
-      if (prems[a] != '') {
-        var w = parseIt(fixWffInputStr(prems[a]));
-        if (!(w.isWellFormed)) {
-          alert('Premise ' + (a+1) + ' is not well formed.');
-          return;
-          }
-        if ((predicateSettings) && (!(w.allFreeVars.length == 0))) {
-          alert('Premise ' + (a+1) + ' is not closed.');
-          return;
-        }
-        sofar.push({
-          "wffstr": wffToString(w, false),
-          "jstr": "Pr"
-        });
-      }
-    }
-    var conc = fixWffInputStr(document.getElementById("probconc").value);
-    var cw = parseIt(conc);
-    if (!(cw.isWellFormed)) {
-      alert('Conclusion is not well formed.');
-      return;
-    }
-    if ((predicateSettings) && (!(cw.allFreeVars.length == 0))) {
-      alert('The conclusion is not closed.');
-      return;
-    }
-    document.getElementById("problabel").style.display = "block";
-    document.getElementById("proofdetails").style.display = "block";
-    var probstr = '';
-    for (var k=0; k<sofar.length; k++) {
-      probstr += prettyStr(sofar[k].wffstr);
-        if ((k+1) != sofar.length) {
-        probstr += ', ';
-      }
-    }
-    document.getElementById("proofdetails").innerHTML = "Construct a proof for the argument: " + probstr + " ∴ " +  wffToString(cw, true);
-    var tp = document.getElementById("theproof");
-    tp.innerHTML = '';
-    makeProof(tp, sofar, wffToString(cw, false));
+    let premisesString = document.getElementById("probpremises").value;
+    let conclusionString = document.getElementById("probconc").value;
+    let proofName = document.getElementById('proofName').value;
+    createProb(proofName, premisesString, conclusionString);
   });
   //end creating a problem based on premise and conclusion
+
+  $('.newProof').click( event => {
+    resetProofUI();
+
+    // Reset 'repoProblem'
+    $('#repoProblem').val('false');
+
+    $('.createProof').slideDown();
+    $('.proofContainer').slideUp();
+  });
 
   $('#proofName').popup({ on: 'hover' });
   $('#repoProofSelect').popup({ on: 'hover' });
@@ -355,3 +372,86 @@ $(document).ready(function() {
   $('.changeInsertMode').click( () => console.log('TODO: enter insert mode') );
   // End admin modal
 });
+
+function resetProofUI() {
+  $('#proofName').val(''); // Clear name
+  $('#tflradio').prop('checked', true); // Set to Propositional
+  $('#probpremises').val(''); // Clear premises
+  $('#probconc').val(''); // Clear conclusion
+  $('.proofNameSpan').text(''); // Clear proof name
+  $('#theproof').empty(); // Remove all HTML from 'theproof' element
+
+  // Reset all select boxes to "Select..." (the first option element)
+  $('#load-container select option:nth-child(1)').prop('selected', true);
+
+  // Remove Logic/Rules from proofContainer's data
+  $('.proofContainer').removeData();
+}
+
+// predicateSettings = (document.getElementById("folradio").checked);
+// var pstr = document.getElementById("probpremises").value;
+// var conc = fixWffInputStr(document.getElementById("probconc").value);
+function createProb(proofName, premisesString, conclusionString) {
+  let pstr = premisesString.replace(/^[,;\s]*/,'');
+  pstr = pstr.replace(/[,;\s]*$/,'');
+  let prems = pstr.split(/[,;\s]*[,;][,;\s]*/);
+  let sofar = [];
+  for (let a=0; a<prems.length; a++) {
+    if (prems[a] != '') {
+      let w = parseIt(fixWffInputStr(prems[a]));
+      if (!(w.isWellFormed)) {
+        alert('Premise ' + (a+1) + ', ' + fixWffInputStr(prems[a]) + ', is not well formed.');
+        return false;
+        }
+      if ((predicateSettings) && (!(w.allFreeVars.length == 0))) {
+        alert('Premise ' + (a+1) + ' is not closed.');
+        return false;
+      }
+      sofar.push({
+        "wffstr": wffToString(w, false),
+        "jstr": "Pr"
+      });
+    }
+  }
+  let conc = fixWffInputStr(conclusionString);
+  var cw = parseIt(conc);
+  if (!(cw.isWellFormed)) {
+    alert('The conclusion ' + fixWffInputStr(conc) + ', is not well formed.');
+    return false;
+  }
+  if ((predicateSettings) && (!(cw.allFreeVars.length == 0))) {
+    alert('The conclusion is not closed.');
+    return false;
+  }
+
+  let proofContainerData = $('.proofContainer').data();
+  if ( proofContainerData.hasOwnProperty('Logic') && proofContainerData.hasOwnProperty('Rules') ) {
+    if ( Array.isArray(proofContainerData.Logic) && Array.isArray(proofContainerData.Rules) ) {
+      proofContainerData.Logic.forEach( (value, idx) => {
+        let w = parseIt(fixWffInputStr(proofContainerData.Logic[idx]));
+        sofar.push({
+          wffstr: wffToString(w, false),
+          jstr: proofContainerData.Rules[idx]
+        })
+      });
+    } else {
+      console.warn('Error/unexpected: Logic/Rules are not arrays', proofContainerData);
+    }
+  }
+
+  $('.createProof').slideUp();
+  resetProofUI();
+  $('.proofContainer').show();
+  $('.proofNameSpan').text(proofName);
+  var probstr = '';
+  for (var k=0; k<sofar.length; k++) {
+    probstr += prettyStr(sofar[k].wffstr);
+      if ((k+1) != sofar.length) {
+      probstr += ', ';
+    }
+  }
+  document.getElementById("proofdetails").innerHTML = "Construct a proof for the argument: " + probstr + " ∴ " +  wffToString(cw, true);
+  var tp = document.getElementById("theproof");
+  makeProof(tp, sofar, wffToString(cw, false));
+  return true;
+}
