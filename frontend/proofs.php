44f78ba9f410e3b91cf4782445bfb6e952ca641d
grbruns <grbruns@gmail.com>
2021-04-01 16:45:46 -0700

    added one of the structural RAA tests

--- frontend/proofs.php
+++ frontend/proofs.php
@@ -900,9 +900,14 @@ function check_proof($pr, $numprems, $conc) {
                 $sp_res1 = $fpr[($fpr[$i]->j->subps[0]->spend - 2)]->wff;     // next to last line of subproof
                 $sp_res2 = $fpr[($fpr[$i]->j->subps[0]->spend - 1)]->wff;     // last line of subproof
                 $res = $fpr[$i]->wff;
+		
 		$sp_res1_loc = $fpr[($fpr[$i]->j->subps[0]->spend - 2)]->location;
 		$sp_res2_loc = $fpr[($fpr[$i]->j->subps[0]->spend - 1)]->location;
 		$res_loc = $fpr[$i]->location;
+
+                if (count($sp_res1_loc) != count($sp_res2_loc)) {
+		   $worked = false;
+		}
                 $worked = followsByRAA2($res, $sp_hyp, $sp_res1, $sp_res2);
                 break;
             case "TND":
