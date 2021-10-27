d7df8b32de8468558e153c138618f3ddcc121e5f
ijabarkhel <ubuntu@ip-172-26-10-23.us-west-2.compute.internal>
2021-10-27 04:42:30 +0000

    Turning off GO111MODULE

--- installer_files/git-hook.sh
+++ installer_files/git-hook.sh
@@ -43,6 +43,7 @@ function updateBackend {
     [[ -f backend/backend ]] && rm backend/backend
     cd backend
     go get github.com/mattn/go-sqlite3
+    export GO111MODULE="off"
     go build backend.go
     if [[ -x ./backend ]]; then
         sudo systemctl stop $1
