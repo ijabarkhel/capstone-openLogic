c688b4e5bc2f7ed5e4e1cf4fe5a5853b33241427
root <root@ip-172-26-10-23.us-west-2.compute.internal>
2021-11-05 06:42:33 +0000

    features added in admin page

--- frontend/admin.js
+++ frontend/admin.js
@@ -1,12 +1,43 @@
 'use strict';
 
 function showAddAdmin() {
-    $('#addStudentToSection').hide();
+    $('#addOrDeleteSection').hide();
+    $('#addOrDeleteStudent').hide();
+    $('#displaySummaryTable').hide();
+    $('#displayTable').show();
     $('#addAdmin').show();
 }
 
 function showAddStudent() {
+    $('#addOrDeleteSection').hide();
     $('#addAdmin').hide();
-    $('#addStudentToSection').show();
+    $('#displaySummaryTable').hide();
+    $('#displayTable').show();
+    $('#addOrDeleteStudent').show();
 }
+
+function showSection() {
+    $('#addOrDeleteStudent').hide();
+    $('#addAdmin').hide();
+    $('#displaySummaryTable').hide();
+    $('#displayTable').show();
+    $('#addOrDeleteSection').show();
+}
+
+function showSummary() {
+    $('#addOrDeleteStudent').hide();
+    $('#addAdmin').hide();
+    $('#addOrDeleteSection').hide();
+    $('#displaySummary').show();
+}
+
+
+$(document).ready(function () {
+
+  $('.displaySummaryBtn').click( () =>  {
+    console.log("Iam in");
+    $('#displaySummaryTable').show();
+  });
+
+});
 
