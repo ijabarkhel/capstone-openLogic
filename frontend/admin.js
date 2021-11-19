45038ff2cf8d64467aee534f3d8f634ab5428d80
root <root@ip-172-26-10-23.us-west-2.compute.internal>
2021-11-19 07:01:09 +0000

    uncommented the backendPost functions

--- frontend/admin.js
+++ frontend/admin.js
@@ -32,18 +32,125 @@ function showSummary() {
 }
 
 function addAdmin() {
-    let adminEmail = $('#adminEmail').text();
-    if (adminEmail) {
-       let userData = new User(adminEmail, adminName, "admin");
+    let userData;
+    let adminEmail = $('#adminEmail').val();
+    let adminName = $('#adminName').val();
+    let addAsAdmin = $('#checkAdmin').val();
+    let addAsInstructor = $('#checkInstructor').val();
 
-       backendPost('addAdmin', userData).then(
+    if (adminEmail && adminName) {
+      if ($('#checkAdmin').is(":checked")){
+	 $('#showError').text('');
+         userData = new User(adminEmail, adminName, addAsAdmin);
+         console.log(addAsAdmin);
+      } else if ($('#checkInstructor').is(":checked")) {
+	 $('#showError').text('');
+         userData = new User(adminEmail, adminName, addAsInstructor);
+         console.log(addAsInstructor);
+      }  else {
+	 $('#showError').text('Error: check admin or instructor');
+      }
+      backendPost('addAdmin', userData).then(
 	(data) => {
-	  console.log('admin added', data);
+	  console.log('admin or instructor added', data);
 	}, console.log)
+    } else {
+	 $('#showError').text('Error: enter admin or instructor email and name to add');
+    }
+}
+function deleteAdmin() {
+    let adminEmail = $('#adminEmail').val();
+    if (adminEmail) {
+      $('#showError').text('');
 
+      //let userData = new User(adminEmail, adminName, "admin");
+
+       backendPost('deleteAdmin', adminEmail).then(
+	(data) => {
+	  console.log('admin or instructor deleted', data);
+	}, console.log)
+    } else {
+	 $('#showError').text('Error: enter admin or instructor email to delete');
     }
 }
 
+function addStudentToSection() {
+    let studentEmail = $('#studentEmail').val();
+    let sectionName = $('#sectionName').val();
+    if (studentEmail && sectionName) {
+      $('#showError2').text('');
+
+      let sectionObject = new Section(studentEmail, sectionName, "student");
+
+      backendPost('addStudentToSection', sectionObject).then(
+	(data) => {
+	  console.log('admin or instructor deleted', data);
+	}, console.log)
+    } else {
+	 $('#showError2').text('Error: enter student email and section name to add');
+    }
+}
+
+function deleteStudentFromSection() {
+    let studentEmail = $('#studentEmail').val();
+    let sectionName = $('#sectionName').val();
+    if (studentEmail && sectionName) {
+      $('#showError2').text('');
+
+       let sectionObject = new Section(studentEmail, sectionName, "student");
+
+       backendPost('deleteStudentFromSection', sectionObject).then(
+	(data) => {
+	  console.log('student deleted from deleted', data);
+	}, console.log)
+    } else {
+	 $('#showError2').text('Error: enter student email and section name to delete');
+    }
+}
+
+function createSection() {
+    let sectionName = $('#sectionName2').val();
+    if (sectionName) {
+      $('#showError3').text('');
+
+      let sectionObject = new Section(studentEmail, sectionName, "student");
+
+      backendPost('createSection', sectionName).then(
+	(data) => {
+	  console.log('section created', data);
+	}, console.log)
+    } else {
+	 $('#showError3').text('Error: enter section name to add');
+    }
+}
+
+function deleteSection() {
+    let sectionName = $('#sectionName2').val();
+    if (sectionName) {
+      $('#showError3').text('');
+
+      backendPost('deleteSection', sectionName).then(
+	(data) => {
+	  console.log('section deleted', data);
+	}, console.log)
+    } else {
+	 $('#showError3').text('Error: enter section name to delete');
+    }
+}
+
+function displaySummary() {
+    let sectionName = $('#sectionName3').val();
+    if (sectionName) {
+      $('#showError4').text('');
+
+      backendPost('getSectionData', sectionName).then(
+	(data) => {
+	  console.log('section data received', data);
+	}, console.log)
+    } else {
+	 $('#showError4').text('Error: enter section name to display summary of the section');
+    }
+}
 
 $(document).ready(function () {
 
@@ -51,6 +158,7 @@ $(document).ready(function () {
     console.log("Iam in");
     $('#displaySummaryTable').show();
   });
+  $('#adminSignIn').show();
 
 });
 
